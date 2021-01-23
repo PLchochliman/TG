9fe0d851fd57b6e9032eead0667b0e6c8af434dc
PLchochliman <72646767+PLchochliman@users.noreply.github.com>
2021-01-23 21:30:17 +0100

    some progress, allocate still not finished

--- alive.py
+++ alive.py
@@ -1,87 +1,89 @@
 """
 it's all about alive objects, which could be destroyed (eg hero, enemy, elephant)
 """
+import system
 
 
+def mod(statystyka):
+    if statystyka < 3:
+        return -2
+    if statystyka < 5:
+        return -1
+    if statystyka < 8:
+        return 0
+    if statystyka < 10:
+        return 1
+    if statystyka < 12:
+        return 2
+    if statystyka == 13:
+        return 3
+    if statystyka > 13:
+        system.Output("coś dużo tego. chyba to potwór \n  Jeśli nie jesteś mutantem, zmień to natychmiast")
+        return 4
 
 
 class IstotaZywa:
 
     sila, zrecznasc, intelekt = 0
-    modSila, modZrecznosc, modInt = 0
+    modSila, modZrecznosc, modIntelekt = 0
     status = true
     drasniecia = 0
     lekkaRana = 0
     powaznaRana = 0
     krytycznaRana = 0
     ranaKonczyny[4] = 0
-
-    def mod(self, statystyka):
-        if statystyka < 3:
-            return -2
-        if statystyka < 5:
-            return -1
-        if statystyka < 8:
-            return 0
-        if statystyka < 10:
-            return 1
-        if statystyka < 12:
-            return 2
-        if statystyka == 13:
-            return 3
-        if statystyka > 13:
-            return false
-
-
-        return True
+    redukcjaObrazen = 0
 
     def __init__(self, Sila, Zrecznasc, Intelekt):
-        sila = Sila
-        zrecznasc = Zrecznasc
-        intelekt = Intelekt
+        self.sila = Sila
+        self.zrecznasc = Zrecznasc
+        self.intelekt = Intelekt
+        self.modSila = mod(Sila)
+        self.modZrecznasc = mod(Zrecznasc)
+        self.modintelekt = mod(Intelekt)
 
-
-    def kill(self):
-        status = false
+    def umarl(self):
+        self.status = false
 
     def allokuj(self, obrazenie):
         if obrazenie == 1:
-            drasniecia =+ 1
+            self.drasniecia =+ 1
         elif obrazenie == 2:
-            lekkaRana =+ 1
+            self.lekkaRana =+ 1
         elif obrazenie == 3:
-            lekkaRana = + 1
+            self.lekkaRana = + 1
         elif obrazenie == 4:
-            lekkaRana = + 1
+            self.lekkaRana = + 1
         elif obrazenie == 5:
-            lekkaRana = + 1
+            self.lekkaRana = + 1
         elif obrazenie == 6:
-            lekkaRana = + 1
+            self.lekkaRana = + 1
 
         return true
 
     def rana(self, rzutNaObrazenia):
+        rzutNaObrazenia = rzutNaObrazenia - self.redukcjaObrazen
         if rzutNaObrazenia <= 1:
-            allokuj(1)
+            self.allokuj(1)
             return True
         elif rzutNaObrazenia == 2:
-            allokuj(2)
+            self.allokuj(2)
             return True
         elif rzutNaObrazenia < 6:
-            allokuj(2)
-            allokuj(rzutNaObrazenia)
+            self.allokuj(2)
+            self.allokuj(rzutNaObrazenia)
             return True
         elif rzutNaObrazenia == 6:
-            allokuj(6)
+            self.allokuj(6)
             return True
         elif rzutNaObrazenia < 11:
-            allokuj(6)
-            allokuj(rzutNaObrazenia)
+            self.allokuj(6)
+            self.allokuj(rzutNaObrazenia)
             return True
         elif rzutNaObrazenia < 15:
-            allokuj(11)
+            self.allokuj(11)
             return True
         elif rzutNaObrazenia >= 15:
-            allokuj(15)
+            self.allokuj(15)
             return True
 
