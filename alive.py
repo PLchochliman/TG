20c37cde7ca1a77fbaf8fd60aab4c9cdef790422
PLchochliman <72646767+PLchochliman@users.noreply.github.com>
2021-01-24 04:17:12 +0100

    Tests are outside libriaries

--- alive.py
+++ alive.py
@@ -4,28 +4,6 @@ it's all about alive objects, which could be destroyed (eg hero, enemy, elephant
 import TGmath
 import system
 
-"""
-mod counts the modificator of statistics
-"""
-"""
-def mod(statystyka):
-    if statystyka < 3:
-        return -2
-    if statystyka < 5:
-        return -1
-    if statystyka < 8:
-        return 0
-    if statystyka < 10:
-        return 1
-    if statystyka < 12:
-        return 2
-    if statystyka == 13:
-        return 3
-    if statystyka > 13:
-        system.Output("coś dużo tego. chyba to potwór \n  Jeśli nie jesteś mutantem, zmień to natychmiast")
-        return 4
-"""
-
 
 class IstotaZywa:
     sila, zrecznasc, intelekt = 0
@@ -62,7 +40,7 @@ class IstotaZywa:
         elif obrazenie == 4:
             self.lekkaRana = + 1
         elif obrazenie == 5:
-            self.ranaKonczyn[system.rollDice(4)] += 1
+            self.ranaKonczyny[system.rollDice(4)] += 1
         elif obrazenie == 6:
             self.powaznaRana = + 1
         elif obrazenie == 7:
@@ -78,8 +56,8 @@ class IstotaZywa:
             self.krytycznaRana = + 1
         elif obrazenie == 15:
             self.umarl()
-
         return True
+
 
     def rana(self, rzutNaObrazenia):
         rzutNaObrazenia = rzutNaObrazenia - self.redukcjaObrazen
@@ -106,4 +84,3 @@ class IstotaZywa:
         elif rzutNaObrazenia >= 15:
             self.allokuj(15)
             return
-
