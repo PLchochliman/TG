a4b449436eaaba5b65829d9aa3109b0f2ba84290
PLchochliman <72646767+PLchochliman@users.noreply.github.com>
2021-06-06 12:40:20 +0200

    preparing for vests

--- przedmioty_ochronne.py
+++ przedmioty_ochronne.py
@@ -2,10 +2,35 @@
 import przedmioty_podstawa as przedmioty_podstawa
 
 
-
-class ubranie(przedmioty_podstawa.Przedmiot):
+class Ubranie(przedmioty_podstawa.Przedmiot):
     kolor = ""
 
+    def __init__(self, czysta_dana):
+        self.kolor = "nico"
 
-class element_szpeju(przedmioty_podstawa.Przedmioty):
+
+class ElementSzpeju(przedmioty_podstawa.Przedmioty):
     kamuflaz = ""
+    magazynki_karabinowe = []
+    magazynki_pistoletowe = []
+    granaty = []
+    apteczka = []
+    maksymalna_pojemnosc = 0
+    zajmowany_slot = ""
+    specjalne = []
+    max_unik = 20
+
+
+    def __init__(self, czysta_dana):
+        super(ElementSzpeju, self).__init__(czysta_dana[-1], czysta_dana[-2])
+        self.kamuflaz = "nico"
+        self.nazwa = czysta_dana[0]
+        self.specjalne = czysta_dana[1]
+        self.max_unik = czysta_dana[2]
+
+        self.maksymalna_pojemnosc = czysta_dana[4]
+        self.zajmowany_slot = czysta_dana[6]
+        magazynki_karabinowe = []
+        magazynki_pistoletowe = []
+        granaty = []
+        apteczka = []
