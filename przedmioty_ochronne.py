f330723af52779e5f895788e497d89316ab03d11
PLchochliman <72646767+PLchochliman@users.noreply.github.com>
2021-06-06 20:02:29 +0200

    preparing for vests, and equipment

--- przedmioty_ochronne.py
+++ przedmioty_ochronne.py
@@ -1,4 +1,3 @@
-
 import przedmioty_podstawa as przedmioty_podstawa
 
 
@@ -18,19 +17,31 @@ class ElementSzpeju(przedmioty_podstawa.Przedmioty):
     maksymalna_pojemnosc = 0
     zajmowany_slot = ""
     specjalne = []
-    max_unik = 20
-
+    maksymakny_unik = 20
+    redukcja = 0
+    mnoznik_cen_plyt = ""
+    typ = ""
 
     def __init__(self, czysta_dana):
         super(ElementSzpeju, self).__init__(czysta_dana[-1], czysta_dana[-2])
         self.kamuflaz = "nico"
         self.nazwa = czysta_dana[0]
         self.specjalne = czysta_dana[1]
-        self.max_unik = czysta_dana[2]
-
+        self.maksymakny_unik = czysta_dana[2]
+        self.redukcja = czysta_dana[3]
         self.maksymalna_pojemnosc = czysta_dana[4]
-        self.zajmowany_slot = czysta_dana[6]
+        self.__obrob_sloty(czysta_dana[5])
+        self.mnoznik_cen_plyt = czysta_dana[6]
         magazynki_karabinowe = []
         magazynki_pistoletowe = []
         granaty = []
         apteczka = []
+
+    def __obrob_sloty(self, wejscie):
+        wejscie = wejscie.split(",")
+        if isinstance(wejscie, list):
+            for i in range(0, len(wejscie)):
+                wejscie[i] = wejscie[i].strip(" ")
+        self.zajmowany_slot = wejscie
+
+
